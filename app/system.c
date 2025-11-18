995d755cde3b8a3d22bd3e461ddeba2715497aee
Amytis Saghafi <amytis25@gmail.com>
2025-11-17 21:15:22 -0800

    created a main file updated color scheme

---
+++ app/system.c
@@ -0,0 +1,19 @@
+#include <stdio.h> // fopen, fprintf, fclose, perror
+#include <stdlib.h>  // exit, EXIT_FAILURE, EXIT_SUCCESS
+#include <stdbool.h>
+#include <time.h>
+#include <string.h>
+#include "doorMod.h"
+
+typedef struct {
+    Door_t door1;
+    char door1_name[50];
+} 
+
+int main(){
+
+
+
+
+    return 0;
+}
